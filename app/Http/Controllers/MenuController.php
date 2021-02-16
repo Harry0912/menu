f8a9d56dc641279fd6db601665ac7dfdc1dc82d2
DESKTOP-M80U7G1\Harry <h7719201@gmail.com>
2021-02-17 02:37:46 +0800

    設定資料庫

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MenuModel;
 
 class MenuController extends Controller
 {
+    public function __construct(MenuModel $menuModel)
+    {
+        $this->MenuModel = $menuModel;
+    }
+
     public function index()
     {
         return view('index');
